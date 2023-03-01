f9e7d1a9fb2b270476a63a14bee63c9758668d69
cjatoba <claytonjatoba@gmail.com>
2023-03-01 07:33:58 -0300

    Mapeia o relacionamento entre as models Person e Contact

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Person extends Model
 {
@@ -16,4 +17,9 @@ class Person extends Model
         'date_of_birth',
         'nationality'
     ];
+
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class);
+    }
 }
